66431c9ef61f21aee99328734c1558e4fd078008
MERT METİN <mertmetin1993@gmail.com>
2024-08-25 23:26:11 +0300

    docfx entegrasyonu

--- src/Nba.WebApi/Controllers/TeamsController.cs
+++ src/Nba.WebApi/Controllers/TeamsController.cs
@@ -19,17 +19,34 @@ namespace Nba.WebApi.Controllers
             _nbaService = new NbaService(settings);
         }
 
-
+        /// <summary>
+        /// Bütün kayıtları getirir.
+        /// </summary>
+        /// <returns><see cref="Teams"/> tipinde bir takım listesini döner.</returns>
         [HttpGet]
         public ActionResult<List<Teams>> Get() =>  _nbaService.GetAll();
 
+        /// <summary>
+        /// Verilen id değerindeki kaydı getirir.
+        /// </summary>
+        /// <param name="id">Takımın id değeri</param>
+        /// <returns><see cref="Teams"/> tipinde takım döner.</returns>
         [HttpGet("{id:length(24)}")]
         public ActionResult<Teams> Get(string id) => _nbaService.GetSingle(id);
 
-
+        /// <summary>
+        /// Yeni takım oluşturur.
+        /// </summary>
+        /// <param name="team">Yeni bir takım nesnesi</param> 
         [HttpPost]
         public ActionResult<Teams> Create(Teams team) => _nbaService.Create(team);
 
+        /// <summary>
+        /// Verilen id değerindeki kaydı günceller.
+        /// </summary>
+        /// <param name="id">Takımın id değeri</param>
+        /// <param name="currentTeam">Güncellenecek kayıt</param> 
+        /// <returns>Kayıt yoksa <see cref="NotFoundResult"/> döner. Başarılıysa <see cref="OkResult"/> döner.</returns>
         [HttpPut("{id:length(24)}")]
 
         public ActionResult Update(string id, Teams currentTeam)
@@ -46,6 +63,11 @@ namespace Nba.WebApi.Controllers
             }
         }
 
+        /// <summary>
+        /// Verilen id değerindeki kaydı siler.
+        /// </summary>
+        /// <param name="id">Takımın id değeri</param>
+        /// <returns>Kayıt yoksa <see cref="NotFoundResult"/> döner. Başarılıysa <see cref="OkResult"/> döner.</returns>
         [HttpDelete("{id:length(24)}")]
         public IActionResult Delete(string id )
         {
